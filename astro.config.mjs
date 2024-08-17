87802c7b58b82ca734a72591273e8ad34d3537d2
moonlitautumn <autumnhazelluna+gay@outlook.com>
2024-08-17 19:03:39 -0300

    edited a file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+
+const LIVE_URL = "https://moonlitautumn.github.io"
+const BASE_URL = "https://localhost:4321"
 // https://astro.build/config
 export default defineConfig({
 
-    site: 'https://moonlitautumn.github.io',
+    site: LIVE_URL,
     base: 'webbb',
 
 
