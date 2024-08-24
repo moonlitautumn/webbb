89bf1016443b2bac6f7540b1cd77ebcff973f520
moonlitautumn <autumnhazelluna+gay@outlook.com>
2024-08-24 17:53:47 -0300

    trying to get oneko working

--- src/components/oneko.js
+++ src/components/oneko.js
@@ -96,7 +96,7 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
     nekoEl.style.zIndex = 2147483647;
 
-    let nekoFile = "https://files.catbox.moe/cjsfx8.gif"
+    let nekoFile = "https://raw.githubusercontent.com/moonlitautumn/webbb/master/src/components/oneko.gif" //https://files.catbox.moe/cjsfx8.gif also works but might be illegal!
     const curScript = document.currentScript
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat
