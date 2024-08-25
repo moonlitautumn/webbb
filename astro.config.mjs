183637919c3be3bdb95958800f756a336bc61abc
moonlitautumn <autumnhazelluna+gay@outlook.com>
2024-08-25 18:25:56 -0300

    this might break stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+const LIVE_URL = "https://moonlitautumn.github.io";
+const BASE_URL = "https://localhost:4321";
 
-
-const LIVE_URL = "https://moonlitautumn.github.io"
-const BASE_URL = "https://localhost:4321"
 // https://astro.build/config
 export default defineConfig({
-
-    site: LIVE_URL,
-    base: 'webbb',
-
-
+  site: LIVE_URL,
+  base: 'webbb',
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
